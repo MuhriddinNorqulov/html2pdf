b65cfb67fb1692db41f20c617d84dbe0b41ee529
muhriddin <1m.norqulov@gmail.com>
2026-02-09 16:23:34 +0500

    add Docker support with Dockerfile and docker-compose.yml; configure server timeouts in echo.go

--- internal/echo.go
+++ internal/echo.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html2pdf/internal/response"
 	myvalidator "html2pdf/internal/validator"
+	"time"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
@@ -13,6 +14,11 @@ import (
 func NewEcho() *echo.Echo {
 	e := echo.New()
 	e.Validator = myvalidator.NewRequestValidator(validator.New())
+
+	e.Server.ReadTimeout = 30 * time.Second
+	e.Server.WriteTimeout = 180 * time.Second // PDF stream uchun katta bo‘lsin
+	e.Server.IdleTimeout = 120 * time.Second
+
 	return e
 }
 
